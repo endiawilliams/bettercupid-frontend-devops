22b21760d2b28d74f3f523ca46b0a0d048d5a1dc
endiawilliams <endiawilliams1@gmail.com>
2020-11-20 17:22:40 -0600

    Fixes header CSS

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserInfo = () => {
     return (
-        <div className="card flex-row flex-wrap">
+        <div className="card flex-row flex-wrap user-info-card">
             <div className="card-header border-0">
                 <img src='https://www.flaticon.com/premium-icon/icons/svg/2102/2102633.svg' height='100px' width='100px' alt='user icon' />
             </div>

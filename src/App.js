faa4a55944e6dc4820baf434339a96d4f32db02f
tbisho <tess.bishop8@gmail.com>
2020-11-22 11:51:44 -0700

    updates functionality for logged in user

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ function App() {
 
   return (
     <div className="App">
-      <Browse />
       <Header 
         currentUser={ currentUser } 
         logout={ logout }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,28 +3,47 @@ import UserInfo from '../components/UserInfo';
 import AboutMe from '../components/AboutMe';
 import ProfileModel from '../models/profile'
 
-const Profile = () => {
-  const [profile, setProfile] = useState([]);
-
-  const fetchProfile = () => {
-    ProfileModel.getOwnProfile().then(data => {
-      console.log(data);
-      setProfile(data.profile)
-    })
-  }
-  console.log(profile)
+// next steps passing props into user info component
+// do a call to the user table within profile controller
+// do another const fetch user in addition to fetchProfile
+const Profile = (props) => {
+  // const [profile, setProfile] = useState("");
+  //  displayName = {
+        // this.setState = displayName
+  // }
+  const [displayName, setDisplayName] = useState("");
+  const [age, setAge] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  // const [state, setState] = useState("");
 
   useEffect(() => {
     fetchProfile()
   }, []);
 
+  const fetchProfile = () => {
+    // console.log(props.currentUser)
+    ProfileModel.getOwnProfile(props.currentUser).then(data => {
+
+      console.log(data.profile.display_name);
+      // setProfile(data.profile)
+      setDisplayName(data.profile.display_name);
+      setAge(data.profile.age);
+      console.log(data.profile)
+      setCity(data.profile.city);
+      setState(data.profile.state);
+    })
+  }
+  
 
   return (
     <div className="profile-container">
-      <UserInfo />
+    
+      <UserInfo displayName={displayName} age={age} city={city} state={state}/>
       <AboutMe />
     </div>
   )
 }
+
 
 export default Profile;

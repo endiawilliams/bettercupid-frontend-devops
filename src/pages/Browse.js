29dcf78c766d529e8ac836e80f753245eb197213
tbisho <tess.bishop8@gmail.com>
2020-11-25 17:45:44 -0700

    displays users on browse page

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,31 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import BrowseResults from '../components/BrowseResults';
 import ProfileModel from '../models/profile';
-
 const ProfilesList = (props) => {
   const [profiles, setProfiles] = useState([]);
-
+  const [displayName, setDisplayName] = useState("");
+  const [age, setAge] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  let displayNames = [];
+  let cities = [];
+  let states = [];
   const fetchProfiles = () => {
     ProfileModel.getAllProfiles().then(data => {
-      console.log(data);
-      setProfiles(data.profiles)
+
+     
+      
+      for (let i = 0; i < 3; i++) {
+        displayNames.push(data.profiles[i].display_name)
+        cities.push(data.profiles[i].city)
+        states.push(data.profiles[i].state)
+      }
+      setDisplayName(displayNames);
+      setCity(cities);
+      setState(states);
+      
+      
     })
   }
-  console.log(profiles)
-
+  
+  console.log(displayName)
+  
   useEffect(() => {
     fetchProfiles()
   }, []);
-
+  
   return (
     <div className="profile-container">
-      <BrowseResults />
-      <BrowseResults />
-      <BrowseResults />
+      <BrowseResults displayName={displayName[0]} city={city[0]} state={state[0]} />
+      <BrowseResults displayName={displayName[1]} city={city[1]} state={state[1]}/>
+      <BrowseResults displayName={displayName[2]} city={city[2]} state={state[2]}/>
     </div>
   );
-
-  
-} 
-
+}
 export default ProfilesList;

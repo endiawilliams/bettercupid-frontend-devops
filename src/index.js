b3f9f199671760eeedaa52d2d14c22997bdbb8f6
endiawilliams <endiawilliams1@gmail.com>
2021-01-27 19:56:02 -0600

    Adds React Snap to fix routing issues.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { hydrate, render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  // StrictMode is a tool for highlighting potential 
-  // problems in an application. Like Fragment, 
-  // StrictMode does not render any visible UI. 
-  // It activates additional checks and warnings for 
-  // its descendants.
-  <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  )
+} else {
+  render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  )
+}
